e17410341c9e660bd7a7d7d44cbc44ff06caf64e
alexkonkin <alexkonkin@gmail.com>
2017-10-04 13:17:08 +0300

    Fixed name of the vue component

---
+++ src/components/misc/UserTripInfoHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label for="icEmail" class="col-md-5 control-label">User Name:</label>
+                <div class="col-md-2">
+                    <input type="text" class="form-control text-center" id="icEmail" placeholder="User Name" :value="name" readonly>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="icSourceStation" class="col-md-5 control-label">Entry point:</label>
+                <div class="col-md-2">
+                    <input type="text" class="form-control text-center" id="icSourceStation" placeholder="Entry Station" :value="sourceStation" readonly>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="icDestinationStation" class="col-md-5 control-label">Destination Point:</label>
+                <div class="col-md-2">
+                    <input type="text" class="form-control text-center" id="icDestinationStation" placeholder="Destination Statioin" :value="destinationStation" readonly>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        computed: {
+        ...mapState({
+            name: state => state.user.name,
+            sourceStation: state => state.sourceStation,
+            destinationStation: state => state.destinationStation
+        }),
+    getUser() {
+        return this.$store.getters.user;
+    }
+    }
+    }
+</script>
+
